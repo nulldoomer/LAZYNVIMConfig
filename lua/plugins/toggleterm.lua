e3a56fdf3960a91b2cc2c7ee39e87f35f507f053
HuesosAce <pauloarielyepez@gmail.com>
2025-02-25 14:24:15 -0500

    fix: bug with the leader key on insert mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,12 +4,14 @@ return {
   config = function()
     require("toggleterm").setup({
       size = 20,
-      open_mapping = [[<leader>t]], -- Atajo para abrir la terminal flotante
-      shade_filetypes = {},
       shading_factor = 2,
-      direction = "float", -- Flotante
+      direction = "float",
       close_on_exit = true,
-      shell = "powershell.exe", -- Usa la shell por defecto
+      shell = "powershell.exe",
     })
+
+    -- Mapeo manual solo en Normal y Visual mode
+    vim.keymap.set({ "n", "v" }, "<leader>t", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
+    vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>:ToggleTerm<CR>]], { noremap = true, silent = true })
   end,
 }
